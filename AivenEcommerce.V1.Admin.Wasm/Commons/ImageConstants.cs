2b00109ad7d32bfa4925b66728d5c5ab86ecec8c
lautaro <lautaroecarro@gmail.com>
2021-01-24 16:36:31 -0300

    add birthday and gender fields in forms

--- AivenEcommerce.V1.Admin.Wasm/Commons/ImageConstants.cs
+++ AivenEcommerce.V1.Admin.Wasm/Commons/ImageConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class ImageConstants
     {
-        public const string IMAGENOTFOUND = @"\assets\images\image-not-found.jpg";
-        public const string PERSONIMAGEDEFAULT = @"\assets\images\user-1633249_960_720.png";
+        public const string IMAGENOTFOUND = "https://aivenweb.com/assets/img/admin/image-not-found.jpg";
+        public const string PERSONIMAGEDEFAULT = "https://aivenweb.com/assets/img/admin/user-photo-default.webp";
     }
 }
